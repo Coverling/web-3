485f0aebd26e12eb3004436e3b730893bd71ffed
Coverling <goracevd2005@gmail.com>
2024-11-04 16:48:51 +0300

    laba

--- projects/star/main.go
+++ projects/star/main.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, world!!!")
+	var str string
+	fmt.Scan(&str)
+	for index, strir := range str {
+		if index == len(str)-1 {
+			fmt.Printf("%s", string(strir))
+			break
+		}
+		fmt.Printf("%s*", string(strir))
+	}
 }
